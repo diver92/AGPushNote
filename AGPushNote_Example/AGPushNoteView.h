a6f4fa3e4031c541b27704c80b970303341cf277
Vlad E. Borovtsov <borovcov.vlad@gmail.com>
2015-01-21 16:53:12 +0600

    interface update

--- AGPushNote_Example/AGPushNoteView.h
+++ AGPushNote_Example/AGPushNoteView.h
@@ -7,6 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "AGPushNoteInfoObj.h"
+
 @protocol AGPushNoteViewDelegate <NSObject>
 @optional
 - (void)pushNoteDidAppear; // Called after the view has been fully transitioned onto the screen. (equel to completion block).
@@ -14,13 +16,13 @@
 @end
 
 @interface AGPushNoteView : UIToolbar
-+ (void)showWithNotificationMessage:(NSString *)message;
-+ (void)showWithNotificationMessage:(NSString *)message completion:(void (^)(void))completion;
++ (void)showWithNotificationObject:(AGPushNoteInfoObj *) obj;
++ (void)showWithNotificationObject:(AGPushNoteInfoObj *) obj completion:(void (^)(void))completion;
 + (void)close;
 + (void)closeWitCompletion:(void (^)(void))completion;
 + (void)awake;
 
-+ (void)setMessageAction:(void (^)(NSString *message))action;
++ (void)setMessageAction:(void (^)(AGPushNoteInfoObj *obj))action;
 + (void)setDelegateForPushNote:(id<AGPushNoteViewDelegate>)delegate;
 
 @property (nonatomic, weak) id<AGPushNoteViewDelegate> pushNoteDelegate;
